8a76a32c4e1b5872c02e4cc7e1deb9bca40ea64c
Daiki Nabuchi <napu1031@gmail.com>
2010-05-29 15:01:32 +0900

    あっぷしたっけ

--- col1.cpp
+++ col1.cpp
@@ -32,6 +32,29 @@ struct GoalLess
     }
 };
 
+struct tblchkret
+{
+    bool hyouka;
+    vector<uint32*>::iterator point;
+};
+
+//後でここtableのポインタで受け取れるように
+//二分木探索で発見できるように
+struct tblchkret tablechk(vector<uint32*> ctab, uint32 target ) {
+    struct tblchkret ret;
+    vector<uint32*>::iterator pp;
+    ret.hyouka = false;
+    ret.point = ctab.begin();
+    for(pp=ctab.begin(); pp!=ctab.end(); ++pp) {
+        if( (*pp)[1] == target ) {
+            ret.hyouka = true;
+            ret.point = pp;
+            return ret;
+        }
+    }
+    return ret;
+}
+
 //Alpha<=1/3 GAMMA=(1-Alpha)/2=1/3
 //N=2^32
 const uint32 N_A = 500;//2048;
@@ -55,75 +78,57 @@ int main()
     }
     sort(table.begin(), table.end(), GoalLess() );
     //goal
-    int count = 0;
-    int t = 1;
+    list<uint32*> retable;
     cout << "後半" << endl;
-    init_genrand( (unsigned long)time(0) );
-    vector<uint32*>::iterator it = table.begin();
-    while( it != table.end() ) {
-        count++;
-        printf( "%u:%u\n", (*it)[0], (*it)[1] );
-        uint32 a = count;//genrand_int32();
+    uint32 t = 1;
+    srand( (unsigned) time(0) );
+    while( t < N_A ){
+        init_genrand( (unsigned long)( time(0)+rand() ) );
+        uint32 a = genrand_int32();
         uint32 b = a;
-        for(uint32 i=0; i<N_R; i++) {
+        for(uint32 j=1; j<=N_R; j++) {
             b = thash(b);
-            if( :
-        }
-        
-        
-        ++it;
-    }
-    return 0;
-/*
-  list<uint32*> table;
-  init_genrand((unsigned long)time(NULL));
-  for(int i=0;i<FUKA;i++) {
-    uint32 s = genrand_int32();
-    uint32 a = s;
-    uint32 l = 0;
-    uint32* factor;
-    while( l<L_MAX ) {
-        a = thash(a);
-        //printf("1:%u:%u:%u\n",a,M,s);
-        l++;
-        if( a < M ) {
-            factor = new uint32[3];
-            factor[0] = s;
-            factor[1] = a;
-            factor[2] = l;    
-            table.push_back(factor);
-            //if (i%100 == 0) {printf("1.5(%d):%u:%u\n",i,a,l); }
-            printf("1.5(%d):%u:%u\n",i,a,l);
-            break;
-        }
-    }
-  }
-    table.sort( GoalLess() );
-    
-    //print
-    list<uint32*>::iterator it = table.begin();
-    while( it != table.end() ) {
-        uint32* tmp = *it;
-        printf("2:%u:%u:%u\n",tmp[0],tmp[1],tmp[2]);
-        ++it;
-    }
-
-    it = table.begin();
-    while( it != table.end() ) {
-        list<uint32*>::iterator jt = it+1;
-        while( jt != table.end() && *jt == *it )
-            ++jt;
-        if( jt >= it+3 ) {
-            // L is maxL
-            uint32 L = 0;
-            list<uint32*>::iterator tt = it;
-            while( tt != jt) {
-                if( L < *tt )
-                    L = *tt;
-                ++tt;
+            struct tblchkret tmp;
+            tmp = tablechk(table,b);
+            bool bol = tmp.hyouka;
+            vector<uint32*>::iterator k = tmp.point;
+            if( bol ) {//tableをチェックしてイテレータkでbが見つかった
+                printf("%u:%u\n", b, (*k)[1]);
+                printf("入った\n");
+                uint32 aa = (*k)[0];
+                //同じ値でマージしてないかチェック
+                
+                for( uint32 l=1; l<=N_R-j; l++ ) {
+                    aa = thash(aa);
+                }
+                
+                //a-b=(*k)[1]-(*k)[0]=aa
+                if( a != aa ) {
+                    b = thash(a);
+                    uint32 bb = thash(aa);
+                    uint32 count = 0;
+                    while( b != bb ) {
+                        count++;
+                        printf("%u:%u  %u\n",b, bb, count);
+                        if( count >= N_R+1 ) { return 0; }
+                        a = b;
+                        aa = bb;
+                        b = thash(a);
+                        bb = thash(aa);
+                    }
+                    uint32 factor[3];
+                    factor[0] = b;
+                    factor[1] = a;
+                    factor[2] = aa;
+                    retable.push_back(factor);
+                    ++t;
+                }
+                continue;
+                printf("出た\n");
             }
-            for( int l=L; l>=0; l-- )
-                for( tt=
-         
-*/
+        }
+    }
+    //tableを出力
+    printf("owari\n");
+   return 0;
 }
